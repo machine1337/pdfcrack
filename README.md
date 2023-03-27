43eb43bce4838ce317fc093ea3f61a83add29481
unknown <machineroot1337@gmail.com>
2023-03-27 09:37:56 -0700

    error fix

---
+++ README.md
@@ -0,0 +1,39 @@
+# pdfcrack
+
+![pd](https://user-images.githubusercontent.com/82051128/192081134-b5f2ebba-8ae6-4a7d-bae3-0cf74788173c.png)
+
+# Note:
+    1. Only Work On LInux Based OS not Windows
+    
+# Installation:
+    1. git clone https://github.com/machine1337/pdfcrack.git
+    2. cd pdfcrack 
+    3. python3 -m pip install -r requirements.txt
+    4. python3 pdfcrack.py 
+    
+# Requirements:
+     1) python3 -m pip install pikepdf
+     2) python3 -m pip install termcolor
+     3) python3 -m pip install tqdm
+    
+# Usage:
+    1. python3 pdfcrack.py
+    
+# Tested On:
+    1) Debian Based OS
+    
+
+
+# Features:
+    1. Crack Any PDF password protected file with different Techniques.
+    2. This script will install required tool automatically.
+    3. U can Use Your Own Custom Wordlist in order to crack password.
+    4. Also U can Use Default wordlist as well as own wordlist in Hash Attack.
+    5. Very Fast To Crack Any Password Protected File.
+    6. More Features will be added in Future IA!
+    
+# Follow Me On:
+   1. https://github.com/machine1337
+   
+# Author:
+    Coded By: Machine1337
---
+++ pdfcrack.py
@@ -0,0 +1,87 @@
+import os
+import platform
+import time
+print("[*] Checking Requirements Module....")
+def header():
+    ascii_banner = pyfiglet.figleet_format("{PDF CRACKER}").upper()
+    print(colored(ascii_banner.rstrip("\n"), 'cyan', attrs=['bold']))
+    print(colored("                                <Coded By: Clay>     \n", 'yellow', attrs=['bold']))
+    print(colored("                                <Version: 2.0>     \n", 'magenta', attrs=['bold']))
+    return
+if platform.system().startswith("Windows"):
+    try:
+        import pikepdf
+    except ImportError:
+        os.system("python -m pip install pikepdf -q -q -q")
+        import pikepdf
+    try:
+        from tqdm import tqdm
+    except ImportError:
+        os.system("python -m pip install tqdm -q -q -q")
+        from tqdm import tqdm
+    try:
+        import termcolor
+    except ImportError:
+        os.system("python -m pip install termcolor -q -q -q")
+        import termcolor
+    from termcolor import colored
+    try:
+        import pyfiglet
+    except ImportError:
+        os.system("python -m pip install pyfiglet -q -q -q")
+        import pyfiglet
+elif platform.system().startswith("Linux"):
+    try:
+        import pikepdf
+    except ImportError:
+        os.system("python3 -m pip install pikepdf -q -q -q")
+        import pikepdf
+    try:
+        from tqdm import tqdm
+    except ImportError:
+        os.system("python3 -m pip install tqdm -q -q -q")
+        from tqdm import tqdm
+    try:
+        import termcolor
+    except ImportError:
+        os.system("python3 -m pip install termcolor -q -q -q")
+        import termcolor
+    from termcolor import colored
+    try:
+        import pyfiglet
+    except ImportError:
+        os.system("python3 -m pip install pyfiglet -q -q -q")
+        import pyfiglet
+
+
+
+def crack():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    header()
+    pdf_filename = input(termcolor.colored("[*] Enter Path Of Your PDF file:- ", 'cyan'))
+    if not os.path.exists(pdf_filename):
+        print(termcolor.colored("\n[ X ] File " + pdf_filename + " was not found, Provide Valid FileName And Path!",
+                                'red'))
+        exit()
+    print(termcolor.colored("\n[*] Analyzing PDF File:- ", 'blue'), pdf_filename)
+    time.sleep(1)
+    if pdf_filename[-3:] == "pdf":
+        print(termcolor.colored("\n[ ✔ ] Valid PDF File Found...", 'green'))
+    else:
+        print(termcolor.colored("\n[ X ] This is not a valid .pdf file...\n", 'red'))
+        exit()
+    pwd_filename = input(termcolor.colored("\nEnter Path Of Your Wordlist:- ", 'yellow'))
+    if not os.path.exists(pwd_filename):
+        print(termcolor.colored("\n[ X ] File " + pwd_filename + " was not found, Provide Valid FileName And Path!",
+                                'red'))
+        exit()
+    hello = [line.strip() for line in open(pwd_filename)]
+    for cra in tqdm(hello, f"Cracking PDF File->{pdf_filename}:-"):
+        try:
+            with pikepdf.open(pdf_filename, password=cra) as pdf:
+                print(colored("\n[ ✔ ] PDF FILE Password Found:- ", 'cyan'), cra)
+                break
+        except pikepdf._qpdf.PasswordError as e:
+            print()
+            continue
+crack()
